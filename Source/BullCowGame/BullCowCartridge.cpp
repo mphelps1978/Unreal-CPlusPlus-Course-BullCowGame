cf955c9d0b0b03565e5f657d4a15ca34c0776cd7
Michael <michaeljphelps1978@gmail.com>
2021-12-17 16:13:17 -0600

    Added basic game loop

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -4,9 +4,24 @@
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
+    PrintLine (TEXT("Welcome to BullCow!"));
+    PrintLine (TEXT("Guess the 4 letter word.")); // TODO: Hardcoded number - Need to refactor for dynamic word length
+    PrintLine (TEXT("Press TAB to begin typing\nType anything and press ENTER"));
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
-
+    // Game Loop
+        // First clear the Screen, THEN set the hidden word, so the user doesn't see it
+    ClearScreen();
+    FString HiddenWord = TEXT("what");
+    
+        // Once the user types a word, we check against the hidden word. 
+        // If they match, you win. If not, you lose.
+    if (Input == HiddenWord) {
+        PrintLine(TEXT("You win!"));
+    } else {
+        PrintLine(TEXT("You Lose!"));
+    }
 }
+
