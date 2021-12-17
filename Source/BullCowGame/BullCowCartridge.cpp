7b5d01c3f7e59c6ebcab039c94bbf9bf457074ad
Michael <michaeljphelps1978@gmail.com>
2021-12-17 16:38:46 -0600

    Elevated HidenWord declaration to private variable in Header, value defined in BeginPlay

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -6,16 +6,15 @@ void UBullCowCartridge::BeginPlay() // When the game starts
     Super::BeginPlay();
     PrintLine (TEXT("Welcome to BullCow!"));
     PrintLine (TEXT("Guess the 4 letter word.")); // TODO: Hardcoded number - Need to refactor for dynamic word length
-    PrintLine (TEXT("Press TAB to begin typing\nType anything and press ENTER"));
+    HiddenWord = TEXT("what");
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
-    // Game Loop
+    // Game Loop TODO: Expand to more functionality (Lives, Letter Tracking, etc..)
         // First clear the Screen, THEN set the hidden word, so the user doesn't see it
     ClearScreen();
-    FString HiddenWord = TEXT("what");
-    
+        
         // Once the user types a word, we check against the hidden word. 
         // If they match, you win. If not, you lose.
     if (Input == HiddenWord) {
