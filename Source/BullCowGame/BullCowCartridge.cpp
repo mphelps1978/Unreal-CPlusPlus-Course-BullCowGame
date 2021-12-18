3c23137e9e9d8547b27ef6b1caf8994a55e781b8
Michael <michaeljphelps1978@gmail.com>
2021-12-17 23:26:47 -0600

    Work Session, see ToDos.md for progress

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -1,26 +1,33 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 #include "BullCowCartridge.h"
 
-void UBullCowCartridge::BeginPlay() // When the game starts
-{
-    Super::BeginPlay();
-    PrintLine (TEXT("Welcome to BullCow!"));
-    PrintLine (TEXT("Guess the 4 letter word.")); // TODO: Hardcoded number - Need to refactor for dynamic word length
-    HiddenWord = TEXT("what");
+void UBullCowCartridge::BeginPlay() {
+  Super::BeginPlay();
+  InitializeGame();
+
+  PrintLine(TEXT("The Hidden Word is %s.\nIt is %i characters long"), *HiddenWord, HiddenWord.Len());  // Debugging
+  PrintLine(TEXT("Welcome to BullCow!"));
+  PrintLine(TEXT("Guess the %i letter word."), HiddenWord.Len());
+  PrintLine(TEXT("You have %i lives left"), Lives);
 }
 
-void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
-{
-    // Game Loop TODO: Expand to more functionality (Lives, Letter Tracking, etc..)
-        // First clear the Screen, THEN set the hidden word, so the user doesn't see it
-    ClearScreen();
-        
-        // Once the user types a word, we check against the hidden word. 
-        // If they match, you win. If not, you lose.
-    if (Input == HiddenWord) {
-        PrintLine(TEXT("You win!"));
-    } else {
-        PrintLine(TEXT("You Lose!"));
+void UBullCowCartridge::OnInput(const FString &Input) {  // When the player hits enter
+
+  ClearScreen();
+
+  // Once the user types a word, we check against the hidden word.
+  // If they match, you win. If not, you lose.
+  if (Input == HiddenWord) {
+    PrintLine(TEXT("You win!"));
+  } else {
+    if (Input.Len() != HiddenWord.Len()) {
+      PrintLine(TEXT("Try again! The Hidden Word is %i characters long."), HiddenWord.Len());
     }
+    PrintLine(TEXT("You Lose!"));
+  }
 }
 
+void UBullCowCartridge::InitializeGame() {
+  HiddenWord = TEXT("what");
+  Lives = 4;
+}
---
+++ Source/BullCowGame/ToDos.md
@@ -0,0 +1,14 @@
+Checking Player Guess:
+  [X]Is Length of Hidden Word?
+  [] Is Isogram?
+  [X]Is Hidden Word?
+  
+Implement Lives System:
+  [X]Starting Lives = Hidden Word Length
+  []On incorrect answer, reduce life by 1
+  []If lives reaches 0, game over
+  
+Fix Hard Coded Values:
+  []Hidden Word  
+  [X]Hidden Word Length
+  [X]Lives
